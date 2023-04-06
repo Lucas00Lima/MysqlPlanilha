53040b3313e9816d17e4e0a047fa764f66dbc538
Lima <117579221+Lucas00Lima@users.noreply.github.com>
2023-04-06 11:10:13 -0300

    Código pronto para uso simples

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,47 @@
 package org.example;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        PercorrerPlanilha percorrerPlanilha =  new PercorrerPlanilha();
-        List<DadosPlanilha> dados = percorrerPlanilha.criar();
-        percorrerPlanilha.imprimir(dados);
+    public static void main(String[] args) throws IOException, SQLException {
+        String url = "jdbc:mysql://localhost:3306/db090";
+        String user = "root";
+        String pass = "@soma+";
+
+        //Estabelece uma conexao com o mysql
+        try (Connection conexao = DriverManager.getConnection(url, user, pass)) {
+            //Comanda Mysql
+            String sql = "INSERT INTO produtos (codigo,nome) VALUES (?,?)";
+            PreparedStatement ps = conexao.prepareStatement(sql);
+
+            //Driver Mysql
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            //Cria a conexão com a planilha e seleciona a primeira aba
+            FileInputStream inputStream = new FileInputStream("C:\\Users\\lukin\\OneDrive\\Área de Trabalho\\Nova Pasta\\planilha.xlsx");
+            XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+            Sheet sheet = workbook.getSheetAt(0);
+
+            //Percorre toda a planilha e transfere os dados
+            for (Row row : sheet) {
+                Cell cell1 = row.getCell(0);
+                Cell cell2 = row.getCell(1);
+                ps.setInt(1, (int) cell1.getNumericCellValue());
+                ps.setString(2, cell2.getStringCellValue());
+                ps.executeUpdate();
+            }
+            System.out.println("Dados inserido");
+        } catch (ClassCastException | ClassNotFoundException ex) {
+            System.out.println("Erro de Conexão");
+        }
     }
 }
--- src/main/java/org/example/PercorrerPlanilha.java
+++ src/main/java/org/example/PercorrerPlanilha.java
@@ -2,9 +2,11 @@ package org.example;
 
 import lombok.Cleanup;
 import org.apache.commons.collections4.IteratorUtils;
-import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -23,23 +25,9 @@ public class PercorrerPlanilha {
         //Setando a aba
         Sheet sheet = workbook.getSheetAt(0);
 
-        //Setando as linhas
+        //Setando as linhas e removendo o cabeçalho
         List<Row> rows = (List<Row>) toList(sheet.iterator());
-
-        int rowcount = sheet.getLastRowNum();
-        System.out.println("Total Row " + rowcount);
-/*
-        for(int i=0;i<rowcount;i++) {
-            String data0 = Cell .setCellType(CellType.NUMERIC);
-            System.out.println("Test Data From Excel" + data0);
-        }
-
-
-*/
-
         rows.remove(0);
-        rows.remove(1);
-        rows.remove(2);
 
         rows.forEach(row ->{
             //Setando as celulas
@@ -47,31 +35,36 @@ public class PercorrerPlanilha {
 
             //Atribuindo os valores
             DadosPlanilha dado = DadosPlanilha.builder()
-                    .código(String.valueOf(cells.get(0).getCellType()))
-                    .categoria(String.valueOf(cells.get(1).getCellType()))
-                    .valor(String.valueOf(cells.get(2).getCellType()))
-                    .origem(String.valueOf(cells.get(3).getCellType()))
-                    .tipo(String.valueOf(cells.get(4).getCellType()))
-                    .obs(String.valueOf(cells.get(5).getCellType()))
-                    .ncm(String.valueOf(cells.get(6).getCellType()))
-                    .cfop(String.valueOf(cells.get(7).getCellType()))
-                    .cest(String.valueOf(cells.get(8).getCellType()))
-                    .cst(String.valueOf(cells.get(9).getCellType()))
-                    .icms(String.valueOf(cells.get(10).getCellType()))
-                    .pisA(String.valueOf(cells.get(11).getCellType()))
-                    .pisC(String.valueOf(cells.get(12).getCellType()))
-                    .cofinsC(String.valueOf(cells.get(13).getCellType()))
-                    .cofinsA(String.valueOf(cells.get(14).getCellType()))
-                    .build();
-            dados.add(dado);
+                .codigo((int)cells.get(0).getNumericCellValue())
+                .categoria(cells.get(1).getStringCellValue())
+                .valor(Integer.parseInt(Integer.toString((int)cells.get(2).getNumericCellValue())))
+                .origem((int)cells.get(3).getNumericCellValue())
+                .tipo((int)cells.get(4).getNumericCellValue())
+                .obs(cells.get(5).getStringCellValue())
+                .ncm(cells.get(6).getStringCellValue())
+                .cfop(cells.get(7).getStringCellValue())
+                .cest(cells.get(8).getStringCellValue())
+                .cst(cells.get(9).getStringCellValue())
+                .icms(cells.get(10).getStringCellValue())
+                .pisC(cells.get(11).getStringCellValue())
+                .pisA(cells.get(12).getStringCellValue())
+                .cofinsC(cells.get(13).getStringCellValue())
+                .cofinsA(cells.get(14).getStringCellValue())
+                .build();
+                dados.add(dado);
         });
         return dados;
     }
-    public List<?> toList(Iterator<?> iterator) {
+    public List<?> toList(Iterator<?> iterator)
+    {
         return IteratorUtils.toList(iterator);
     }
-    public void imprimir(List<DadosPlanilha> dado) {
+
+    //Percorre a array e imprime
+    public void imprimir(List<DadosPlanilha> dado)
+    {
         dado.forEach(System.out::println);
     }
 }
+
 
